2218aaa175f8247f604347f926dfce5422a65a71
Alisa <agvyshegorodtseva@edu.hse.ru>
2021-12-13 22:17:50 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/avs_threads.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avs_threads.dir/main.cpp.o"
+  "CMakeFiles/avs_threads.dir/random.cpp.o"
+  "CMakeFiles/avs_threads.dir/shooter.cpp.o"
+  "avs_threads"
+  "avs_threads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avs_threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
